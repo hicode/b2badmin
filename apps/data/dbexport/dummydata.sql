b58a44dc9d2bfe000b81b93b3daa2985fdf936d0
ppantelakis <panagiotis.pantelakis@gmail.com>
2016-12-15 01:04:29 +0200

    Commit 3

---
+++ apps/data/dbexport/dummydata.sql
@@ -0,0 +1,4 @@
+UPDATE b2badminschema.core_crm_customer
+   SET lname=fname, telmobile='111111111', telland='111111111', 
+       birthday='111111111', email='test@test.gr', afm='111111111111',  addressname1='1111111111111111111'
+ WHERE id=id;
